0cf7cc6361715a719942966bf6c4a2b755e53df9
Seladus <clement.blancovolle@insa-rouen.fr>
2021-02-14 14:43:22 +0100

    Correcting sqlite support

--- create.sql
+++ create.sql
@@ -15,11 +15,7 @@ CREATE TABLE sessions (
     start_timestamp INTEGER NOT NULL,
     end_timestamp INTEGER NOT NULL,
     is_distance INTEGER NOT NULL, 
-    length INTEGER GENERATED ALWAYS AS (end_timestamp - start_timestamp) STORED,
-    FOREIGN KEY (user_id)
-        REFERENCES users (user_id)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE
+    length INTEGER NOT NULL
 );
 
 /* INSERT INTO users (user_name, email, weekly_work_time) VALUES ("username", "email", <time_to_work>); */
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 systemctl disable pic-check-in
+cp /usr/local/bin/pic-check-in/ressources/users-data/Data.db ~/Data.db
 rm -rf /etc/systemd/system/pic-check-in.service
 rm -rf /usr/local/bin/pic-check-in/
