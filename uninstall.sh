67c3a81fa6bd33dfca32981d62f621b10fc3c6a1
Seladus <clement.blancovolle@insa-rouen.fr>
2021-02-14 14:47:45 +0100

    ui

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 systemctl disable pic-check-in
-cp /usr/local/bin/pic-check-in/ressources/users-data/Data.db ~/Data.db
 rm -rf /etc/systemd/system/pic-check-in.service
 rm -rf /usr/local/bin/pic-check-in/
